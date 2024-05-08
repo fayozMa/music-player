a61b8b339dca7c38901dd9c010f06d92967a6045
Muhammadfayyozbek Ahmadjonov <usmonovbahriddin108@gmail.com>
2024-05-08 14:35:12 +0500

    [FIX] backward and forward are working

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@ const playBtn = document.getElementById("playBtnToggle")
 const audioEl = document.getElementById("audio")
 const coverEl = document.getElementById("cover")
 const player = document.getElementById("player")
+const backwardEL = document.getElementById("backward")
+const forwardEL = document.getElementById("forward")
 const changeVolume = document.getElementById("changeVolume")
-const tracks = [ 'allMyFriendAreToxic','bilmaydi-konsta','chumoli-konsta','daylight','falling','hope','insonlar-konsta','jujutsuKaisenOP1','mood','novacain','revenge','smackThat','sunflower','suzume','sweaterWeather','until_i_found_you']
+const tracks = [ 'allMyFriendAreToxic','bilmaydi-konsta','chumoli-konsta','daylight','falling','hope','insonlar-konsta','jujutsuKaisenOP1','mood','novacain','revenge','sunflower','suzume','sweaterWeather','untilIFoundYou']
 let currentTrack = 0
 const changeTrack = (index) => {
     audioEl.src = `./musics/${tracks[index]}.mp3`
@@ -24,4 +26,23 @@ playBtn.addEventListener("click", () => {
 changeVolume.addEventListener("input" , (e) => {
     audioEl.volume = e.target.value
 })
+const plas = () => {
+    currentTrack = (currentTrack + 1) % tracks.length;
+    changeTrack(currentTrack);
+    audioEl.play();
+    playBtn.classList = "fa-solid fa-pause"
+}
+const minus = () => {
+    currentTrack = (currentTrack - 1 + tracks.length ) % tracks.length;
+    changeTrack(currentTrack);
+    audioEl.play();
+    playBtn.classList = "fa-solid fa-pause"
+}
 
+backwardEL.addEventListener('click', () => {
+    plas();
+});
+
+forwardEL.addEventListener('click', () => {
+    minus();
+});
